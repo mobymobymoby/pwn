137ae077a715163c3d414a53b2f4181bf565c787
정재영 <43400316+J-jaeyoung@users.noreply.github.com>
2021-06-20 10:16:46 +0900

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,8 +3,8 @@ set -ex
 
 apt update
 apt install vim git curl wget build-essential tmux gdb -y
-apt install python2.7
-apt install pip-python
+apt install python2.7 -y
+apt install pip-python -y
 git clone https://github.com/pwndbg/pwndbg
 cd pwndbg
 ./setup.sh
